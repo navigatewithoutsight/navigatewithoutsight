a137116a31e0d583add0c0c96085f3a248f2777d
Sbbrignone <s.sbrignone@gmail.com>
2026-01-23 22:52:29 +0100

    Revert "Update real_main.c"
    This reverts commit a444f323a09523ce19501f352ddaf43cf2bb01c8.

--- main/real_main.c
+++ main/real_main.c
@@ -1,25 +1,36 @@
 #include "real_main.h"
 #include "buzzer.h"
-#include "button.h"
 #include "esp_log.h"
+#include "esp_timer.h"
+#include "freertos/idf_additions.h"
 #include "gy87.h"
 #include "ssd1306_simple.h"
 #include "tof.h"
-#include "vl53l1x.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
-static const char *ALL_MODULES_MAIN = "TOF";
-static bool system_active = false; // start OFF
+#define I2C_SDA_NUM 21
+#define I2C_SCL_NUM 22
+static const char *ALL_MODULES_MAIN = "REAL_MAIN";
 
-
+// This is main_main function
+// STEPS:
+// 1. Init bus. VL lib does it
+// 2. Init mcu
+// 3. loop
 void all_modules_main() {
-  // STEPS:
-  // 1. Init bus. VL lib does it
-  // 2. Init mcu
-  // 3. loop
+  SemaphoreHandle_t i2c_mutex;
+  i2c_mutex = xSemaphoreCreateMutex();
   vl53l1x_t *dev = NULL;
 
+  vTaskDelay(pdMS_TO_TICKS(100));
+  dev = vl53l1x_config(0,           // port
+                       I2C_SCL_NUM, // scl
+                       I2C_SDA_NUM, // sda
+                       -1,          // xshut (not used)
+                       0x29,        // I2C address
+                       0            // io_2v8
+  );
+
+  // vTaskDelay(pdMS_TO_TICKS(400));
   ESP_LOGI(ALL_MODULES_MAIN, "Start...");
   int err;
   err = tof_init(dev);
@@ -27,6 +38,7 @@ void all_modules_main() {
     ESP_LOGI(ALL_MODULES_MAIN, "TOF init failed...");
     return;
   }
+  vTaskDelay(pdMS_TO_TICKS(100));
   err = GY87_init_no_i2c_bus();
   if (err) {
     ESP_LOGI(ALL_MODULES_MAIN, "TOF init failed...");
@@ -34,53 +46,59 @@ void all_modules_main() {
   }
 
   ESP_LOGI(ALL_MODULES_MAIN, "Init phase passed...");
+  vTaskDelay(pdMS_TO_TICKS(100));
   ssd1306_init();
   buzzer_init();
+  buzzer_set_mode_event(1); // startup beep
 
-  button_config_t btn = { .gpio = 32, .debounce_ms = 50 };
-  button_init(&btn);
-
-
-  //buzzer_set_mode_event(1); // startup beep
-  
-
+  int64_t start = esp_timer_get_time();
+  vTaskDelay(pdMS_TO_TICKS(100));
   float gz; // store z-axis degree/sec readings, updates each loop
+  int turn_dir = 0;
+  int turn_deg = 0;
+  int spm = 0;
+  int speed_x100 = 0;
+  float speed = 0;
+  float cadence = 0;
   int iteration_error = 0; // 1 is error, 0 is success
   int event = 0;           // 1 is error, 0 is success
   int direction = 0;       // 1 is error, 0 is success
-  int distance = 999;
+
   while (1) {
+    int64_t end = esp_timer_get_time();
+    ESP_LOGI(ALL_MODULES_MAIN, "Iteration took microsecs: %d",
+             (uint)(end - start));
+    gy87_loop_iteration(&cadence, &speed, &gz, i2c_mutex);
+    spm = (int)(cadence + 0.5f);
+    speed_x100 = (int)(speed * 100.0f + 0.5f);
 
-    if (button_was_pressed()) {
-      system_active = !system_active;
-      buzzer_set_mode_event(system_active ? 1 : 2); // ON beep / OFF beep
-    }
+    tof_loop_iteration(dev, i2c_mutex);
+    ESP_LOGI(ALL_MODULES_MAIN, "Single-shot result: %u mm",
+             dev->ranging_data.range_mm);
+    ESP_LOGI(ALL_MODULES_MAIN, "(%.2f °/s)", gz);
+    ESP_LOGI(ALL_MODULES_MAIN, "Cadence: %.1f spm | Estimated speed: %.2f m/s",
+             cadence, speed);
 
-    if (!system_active) {
-      // keep quiet while OFF
-      buzzer_set_error(0);
-      buzzer_set_distance_cm(999, 0);
-      buzzer_iteration();
+    if (gz > 20.0f)
+      turn_dir = 1;
+    else if (gz < -20.0f)
+      turn_dir = -1;
+    else
+      turn_dir = 0;
+    buzzer_set_distance_cm(dev->ranging_data.range_mm, 1);
+    buzzer_set_error(0);
 
-      vTaskDelay(pdMS_TO_TICKS(50));
-      continue;
-    }
+    turn_deg = (int)gz;
 
-
-    ESP_LOGI(ALL_MODULES_MAIN, "Iteration starts...");
-    gy87_loop_iteration(&gz);
-    distance = tof_loop_iteration(dev);
-    /*// Testing: cahnge with real distance from ToF
-    buzzer_set_distance_cm(999, 1);  // 999cm = far away, valid reading
-    buzzer_set_error(0);            // no error for now
-  */
-    ssd1306_render_dashboard(0, // turn_dir (LEFT / RIGHT / NONE) → placeholder
-                             0, // turn_deg
-                             0, // steps per minute
-                             0, // speed * 100
-                             NULL, // depth
-                             0);
-    buzzer_iteration_main(iteration_error, event, direction, distance);
-    ESP_LOGI(ALL_MODULES_MAIN, "Iteration ends...");
+    ssd1306_render_dashboard(
+        turn_dir,   // turn_dir (LEFT / RIGHT / NONE) → placeholder
+        turn_deg,   // turn_deg
+        spm,        // steps per minute
+        speed_x100, // speed * 100
+        dev->ranging_data.range_mm,
+        i2c_mutex);
+    buzzer_iteration_main(iteration_error, event, direction,
+                          dev->ranging_data.range_mm);
+    start = end;
   }
 }
